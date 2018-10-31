cb6ee59ba5ec91f95b460055e9d74ce5c3cfc81a
paladinze <paladinze@hotmail.com>
2018-10-31 16:56:41 +0800

    test

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=5 color=#2699BF icon="\uf110"
+namespace rgb {
+    //% block="show color $color"
+    //% color.shadow="colorNumberPicker"
+    export function showColor(color: number) {
+
+    }
+
+    //% block="show wheel $color"
+    //% color.shadow="colorWheelPicker"
+    export function showColorWheel(color: number) {
+
+    }
+
+    //% block="show wheel hsv $color"
+    //% color.shadow="colorWheelHsvPicker"
+    export function showColorWheelHsv(color: number) {
+
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `);
