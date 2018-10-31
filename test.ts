7f7edae88bc9d2ed42f809bb2c685e4ffa1cf279
paladinze <paladinze@hotmail.com>
2018-11-01 01:24:10 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,10 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
-
-IR.init(Pins.P1)
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+IR.init(Pins.P15)
+IR.onPressEvent(69, function () {
+    basic.showIcon(IconNames.Heart)
+})
