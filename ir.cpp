70e0abef900817e96706f402c51cfb9ac8c3f333
paladinze <paladinze@hotmail.com>
2018-10-31 17:07:44 +0800

    test

--- ir.cpp
+++ ir.cpp
@@ -1,1 +1,17 @@
+#include "pxt.h"
+using namespace pxt;
 
+//% color="#D063CF" weight=19 icon="\uf1eb"
+namespace IR {
+
+  /**
+  * initialization
+  */
+  //% blockId=ir_init
+  //% block="connect IR to %pin"
+  void init(Pins pin){
+
+  }
+
+
+}
